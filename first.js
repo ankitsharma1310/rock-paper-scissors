c186018193b6dfbd1558c7f6c4dce17b8272bb90
AnkiT sharma <ankitsharma.1310as@gmail.com>
2024-06-27 16:17:25 +0530

    Complete game

---
+++ first.js
@@ -0,0 +1,74 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play again.";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userChoice) => {
+  // Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    // Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+// Function to reset game
+const resetGame = () => {
+  userScore = 0;
+  compScore = 0;
+  userScorePara.innerText = '0';
+  compScorePara.innerText = '0';
+  msg.innerText = 'Play your move';
+  msg.style.backgroundColor = '#081b31';
+};
+
+// Event listener for restart button
+document.getElementById('restart-btn').addEventListener('click', resetGame);
+
+// Event listeners for game choices
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
